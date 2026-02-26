d4889df2b4c1f6755938e08b3ab5afbc5b9a4d8a
Utkarsh3904 <millenniumatom@gmail.com>
2026-02-26 23:37:34 +0530

    Add custom not found page for blog and general use

---
+++ app/(application)/blogs/[blogID]/not-found.js
@@ -0,0 +1,12 @@
+//Custom not found page for blog specifics
+
+
+export default function BlogNotFound() { //we dont get any aprams here 
+  return (
+      <>
+      <h1>Blog Page not found </h1>
+      <p>Could not found the page you are looking for </p>
+      </>
+  )
+}
+
---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+//custom not found page
+
+export default function() {
+  return (
+      <>
+      <h1>Page not found </h1>
+      <p>Could not found the page you are looking for </p>
+      </>
+  )
+}
+
