8ab1ea4ff774afb2b210e654525abec217d1668c
Utkarsh3904 <millenniumatom@gmail.com>
2026-02-28 23:49:08 +0530

    Add loading component to display loading state during page delays

---
+++ app/todos/loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div>
+      loading ...
+      this will show up when its neighbour page shows any delay its this code is enough
+    </div>
+  )
+}
+
+export default loading
