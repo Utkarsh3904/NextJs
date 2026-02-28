0128bd6167b9c6fccd98d788169e8582a0592975
Utkarsh3904 <millenniumatom@gmail.com>
2026-02-28 23:49:19 +0530

    Add initial page component for todos with placeholder text

---
+++ app/todos/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        simple page. make fetch call of any api either for server or for client
+    </div>
+  )
+}
+
+export default page
