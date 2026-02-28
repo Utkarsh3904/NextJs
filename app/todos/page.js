5c08c8484404422ac19921676a377e242e14787a
Utkarsh3904 <millenniumatom@gmail.com>
2026-03-01 00:16:11 +0530

    Add commented-out promise for simulating fetch delay in todos page

--- app/todos/page.js
+++ app/todos/page.js
@@ -3,6 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div>
+    //await new Promise((resolve) => setTimeout (resolve,5000); 
         simple page. make fetch call of any api either for server or for client
     </div>
   )
